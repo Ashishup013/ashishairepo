2dc272027fc4cdf41556e8e72b59e23c8ebc8451
ashish <ashishup013@gmail.com>
2025-12-14 21:18:59 +0530

    different condition

--- Test.py
+++ Test.py
@@ -1,6 +1,22 @@
+import datetime;
 msg="ashish";
 print(msg);
 
 msg1="hello world"
 print(msg1);
 
+fruit =["apple","banana","mango"]
+for c in fruit:
+  print(c)
+
+for z in range(1,5):
+  print(z)
+
+# array emaple#
+fruit=["banana","mango","apple","grapes"]
+print(fruit[0]);
+x=len(fruit);
+print(x)
+z=datetime.datetime.now();
+print(z)
+#conditional operator#
